15334216cef0bfe9c9bd61b01dafc41f35aa500a
Tan He Song <55863140+tan7622@users.noreply.github.com>
2020-12-26 12:41:05 +0800

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+  <h1>Details</h1>
+  <p>My address</p>
+  <p>0174317622</p>
+  <p>tanhesong66@gmail.com</p>
+  <hr>
+  <form class="" action="index.php" method="post">
+        <label>Name:</label>
+        <input type="text" name="name" value=""><br>
+        <label>Email:</label>
+        <input type="email" name="email" value=""><br>
+        <label>Your Message:</label><br>
+        <textarea name="message" cols="30" rows="10"></textarea><br>
+        <input type="submit" name="">
+
+        </form>
+			</body>
+		</html>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,79 @@
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Udemy</title>
+			</head>
+			<body>
+				<h1>Tan's family</h1>
+				<img src="https://thumbs-prod.si-cdn.com/Mj9M93ipIh6i91snW9LmlxZhPyw=/800x600/filters:no_upscale():focal(634x85:635x86)/https://public-media.si-cdn.com/filer/7b/ba/7bba298e-7e2e-44f0-adb9-b47dfdc1e240/p05m69vt.jpg" alt="Tan's Famnily profile pictures">
+					<P>
+						<em>A family who love each other 
+							<strong>Everyday</strong>.
+						</em>
+					</P>
+					<hr>
+						<h3>Knowledge</h3>
+						<!-- BulletPoint list -->
+						<ul>
+							<li>House Keeping</li>
+							<li>
+								<a href="https://www.ubisoft.com/en-gb/game/rainbow-six/siege">Gaming</a>
+							</li>
+							<li>Learning</li>
+							<li>Problem solver</li>
+						</ul>
+						<h3>
+							<a href="page2.php">Hobby</a>
+						</h3>
+						<!-- Orderlist -->
+						<ol type ="i">
+							<li>Hiking</li>
+							<li>Gaming</li>
+							<li>Coding</li>
+							<li>Jogging</li>
+						</ol>
+						<hr>
+							<h3> Work Experience</h3>
+							<table>
+								<thead>
+									<tr>
+										<th>Work</th>
+										<th>Dates</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>Computer Scientist</td>
+										<td>2015-2020</td>
+									</tr>
+									<tr>
+										<td>Computer Engineering</td>
+										<td>2000-2012</td>
+									</tr>
+								</tbody>
+							</table>
+							<hr>
+								<h3>Skills</h3>
+                <table>
+                <tr>
+                <td>
+                <table cellspacing ="10">
+									<tr>
+										<td>PHP</td>
+										<td>⭐⭐⭐⭐</td>
+									</tr>
+									<tr>
+										<td>JAVA</td>
+                    <td>⭐⭐</td>
+									</tr>
+                  <tr>
+										<td>C++</td>
+                    <td>⭐</td>
+									</tr>
+								</table>
+              </table>
+                <hr>
+                 <a href="contact.php"><h3>Contact</h3></a>
+							</body>
+						</html>
